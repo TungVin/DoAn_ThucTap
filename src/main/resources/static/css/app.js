d7992534d588b27a3712eb9038f6efb79a47c268
Nguyen Tung Vi <vimatau@gmail.com>
2025-11-25 16:55:30 +0700

    Cập nhật thay đổi

---
+++ src/main/resources/static/css/app.js
@@ -0,0 +1,9 @@
+// Hiệu ứng nút đăng nhập
+document.querySelector(".login-btn").addEventListener("click", () => {
+    alert("Chức năng đăng nhập đang phát triển!");
+});
+
+// Hiệu ứng chuông
+document.querySelector(".bell").addEventListener("click", () => {
+    alert("Bạn không có thông báo mới.");
+});
